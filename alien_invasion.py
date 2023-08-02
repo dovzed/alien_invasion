2fb409802df53c4501af85c384784ca39850ecc1
Dov Zabrowsky <dovzab1@gmail.com>
2023-08-02 11:33:02 -0400

    fullscreen_mode

--- alien_invasion.py
+++ alien_invasion.py
@@ -13,8 +13,9 @@ class AlienInvasion:
         pygame.init()
         self.clock = pygame.time.Clock()
         self.settings = Settings()
-        self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height))
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_height = self.screen.get_rect().height
+        self.settings.screen_width = self.screen.get_rect().width
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
 
